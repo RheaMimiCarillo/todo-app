ce5e16fd8e645dc1deb633be93eabaebbe8abfdc
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-11-07 16:34:59 +0000

    added <AuthProvider> to index.js

--- src/components/SettingsPage/SettingsPage.jsx
+++ src/components/SettingsPage/SettingsPage.jsx
@@ -11,9 +11,8 @@ import
   DrawerSize,
   HTMLSelect,
 } from "@blueprintjs/core";
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
-import { useContext } from 'react';
 // note how this is imported with deconstruction
 import { SettingsContext } from '../../context/SettingsContext';
 
